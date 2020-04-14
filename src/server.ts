87b30b08d7cddc9192eb28d5fb8969bcd93d8674
Pedro Leinar <pedroleinar@hotmail.com>
2020-04-14 03:41:20 -0300

    gobarber

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,7 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-
+app.use(express.json());
 app.use(routes);
 
-
-app.listen(3333, () => {
-  console.log('Server started on port 3333');
-});
+app.listen(3333);
