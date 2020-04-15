9dbb388d633245d370e7ae2ee46f3dd9057b5fb9
Pedro Leinar <pedroleinar@hotmail.com>
2020-04-15 01:12:34 -0300

    migrations

---
+++ src/database/migrations/1586923904630-CreateAppointments.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointments1586923904630 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {}
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
+
 app.use(express.json());
 app.use(routes);
 
