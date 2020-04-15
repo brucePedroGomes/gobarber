086b125cd51ca0cbd0c78ae002dfb347b5b7f3c0
Pedro Leinar <pedroleinar@hotmail.com>
2020-04-15 04:08:37 -0300

    typeorm

--- src/database/migrations/1586923904630-CreateAppointments.ts
+++ src/database/migrations/1586923904630-CreateAppointments.ts
@@ -1,7 +1,35 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1586923904630 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<any> {}
+export default class CreateAppointments1586923904630
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<any> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
 }
