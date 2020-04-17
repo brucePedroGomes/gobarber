6e681289ed731e585c1fdac9ee8862752581230a
Pedro Leinar <pedroleinar@hotmail.com>
2020-04-17 00:17:39 -0300

    jwt

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { getRepository } from 'typeorm';
+import CreateUserService from '../services/CreateUserService';
+import User from '../models/User';
+
+const usersRouter = Router();
+
+usersRouter.get('/', async (request, response) => {
+  const usersRepository = getRepository(User);
+
+  const users = await usersRepository.find();
+
+  return response.json(users);
+});
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
