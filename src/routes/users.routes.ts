d86fed0c5051a13643fc5087648ea4ed956e423c
Pedro Leinar <pedroleinar@hotmail.com>
2020-04-17 22:15:55 -0300

    uploadImage

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
+
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
 import User from '../models/User';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', async (request, response) => {
   const usersRepository = getRepository(User);
@@ -31,5 +37,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
