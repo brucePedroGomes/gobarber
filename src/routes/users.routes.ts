16ff5c29107d2614c3404546eb749b5b0ce41335
Pedro Leinar <pedroleinar@hotmail.com>
2020-04-18 00:17:49 -0300

    avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,6 +5,8 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import User from '../models/User';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -43,8 +45,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
