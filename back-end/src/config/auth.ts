6899fb73c148190cff131a6019c2517d64de507f
Pedro Gomes <pedroleinar@hotmail.com>
2020-05-27 20:34:05 -0300

    folders

--- back-end/src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: 'uehu23h4u2hudsh$$$',
-    expiresIn: '7d',
-  },
-};
--- back-end/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++
@@ -1,9 +0,0 @@
-import IMailTemplateProvider from '../models/IMailTemplateProvider';
-
-class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse(): Promise<string> {
-    return 'Mail Content';
-  }
-}
-
-export default FakeMailTemplateProvider;
