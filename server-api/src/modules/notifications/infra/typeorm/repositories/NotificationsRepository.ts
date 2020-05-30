6372d1af7b4a1ceef50cd81a38e9ee515f1c1001
Pedro Gomes <pedroleinar@hotmail.com>
2020-05-30 03:48:08 -0300

    notifications-repository-mongodb

---
+++ server-api/src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,30 @@
+import { MongoRepository, getMongoRepository } from 'typeorm';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+
+import ICreateNotificationsDTO from '@modules/notifications/dtos/ICreateNotificationsDTO';
+import Notification from '../schemas/Notification';
+
+class NotificationsRepository implements INotificationsRepository {
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    recipient_id,
+    content,
+  }: ICreateNotificationsDTO): Promise<Notification> {
+    const notification = this.ormRepository.create({
+      recipient_id,
+      content,
+    });
+
+    await this.ormRepository.save(notification);
+
+    return notification;
+  }
+}
+
+export default NotificationsRepository;
