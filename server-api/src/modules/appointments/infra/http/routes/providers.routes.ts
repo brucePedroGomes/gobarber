f37078694cc60bebeb19d70a2d4fb1936df2c618
Pedro Gomes <pedroleinar@hotmail.com>
2020-05-30 05:38:11 -0300

    celebrate

--- server-api/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ server-api/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import ProvidersController from '../controllers/ProvidersController';
@@ -18,10 +18,21 @@ providersRouter.get('/', providersController.index);
 
 providersRouter.get(
   '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerMonthController.index,
 );
+
 providersRouter.get(
   '/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerDayController.index,
 );
 
--- server-api/src/modules/users/infra/http/routes/profile.routes.ts
+++ server-api/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ProfileController from '../controllers/ProfileController';
@@ -9,7 +10,19 @@ const usersController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.get('/', usersController.show);
+profileRouter.get(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  usersController.show,
+);
 profileRouter.put('/', usersController.update);
 
 export default profileRouter;
